cbbb76c5f74b51f52df8430f0ff05127677e58dc
Wind Li <nightwend@163.com>
2023-02-18 16:28:17 +0800

    执行成功，并包含报告链接

--- readme.md
+++ readme.md
@@ -1,2 +1,17 @@
 # 阿里云容器镜像服务  
-https://cr.console.aliyun.com/cn-beijing/instances
+https://cr.console.aliyun.com/cn-beijing/instances  
+
+# 云效流水线自定义步骤  
+https://help.aliyun.com/document_detail/153811.html?spm=a2cl9.flow_devops2020_goldlog_detail.0.0.45f146f6321yVX  
+
+# 上传报告  
+需要自己添加上传报告步骤，报告路径 robot_logs, 文件名 report.html  
+
+# 测试命令  
+```
+robot testfolder
+```
+或者
+```
+pabot testfolder
+```
--- robotframework-browser-flow-wl/step.sh
+++ robotframework-browser-flow-wl/step.sh
@@ -11,6 +11,7 @@ echo [INFO] WORK_SPACE=$WORK_SPACE         # /root/workspace容器中目录
 echo [INFO] PROJECT_DIR=$PROJECT_DIR       # 代码库根路径，默认为/root/workspace/code
 echo [INFO] PLUGIN_DIR=$PLUGIN_DIR         # 插件路径，默认为/root/workspace/plugins
 echo [INFO] BUILD_JOB_ID=$BUILD_JOB_ID     # build-service 任务ID
+echo [INFO] TESTCOMMAND=$TESTCOMMAND       # 测试命令
 
 # custom variable
 OUTPUT_DIR="$PROJECT_DIR/robot_logs"
@@ -21,14 +22,15 @@ cd $PROJECT_DIR
 
 # sh -ex $WORK_SPACE/user_command.sh
 # bash -c "$STEP_COMMAND"
-/root/entry_point.sh $command
+/root/entry_point.sh $TESTCOMMAND
 
-# 上传报告
-# walk through robot_logs to upload
-output=`python3 /root/upload_report.py $OUTPUT_DIR`
-printf $output
-# 获取报告链接
-STEP_REPORT_URL=`echo $output | grep "STAT_URL__REPORT" | awk -F, '{print $2}'`
+# # 上传报告
+# # walk through robot_logs to upload
+# output=`python3 /root/upload_report.py $OUTPUT_DIR`
+# printf $output
+# # 获取报告链接
+# STEP_REPORT_URL=`echo $output | grep "STAT_URL__REPORT" | awk -F, '{print $2}'`
+STEP_REPORT_URL=https://flow.aliyun.com/assets/$SYSTEM_REGION_ID/$PIPELINE_ID/$BUILD_NUMBER/robot_logs/log.html
 printf "STEP_REPORT_URL: $STEP_REPORT_URL"
 
 output=`python3 /root/parse_output.py $OUTPUT_XML`
@@ -37,4 +39,4 @@ STEP_ROBOT_PASS=`echo $output | awk -F, '{print $1}'`
 STEP_ROBOT_FAILED=`echo $output | awk -F, '{print $2}'`
 STEP_ROBOT_PASSRATE=`echo $output | awk -F, '{print $3}'`
 
-redline Passed:成功:$STEP_ROBOT_PASS:Success Failed:失败:$STEP_ROBOT_FAILED:Error PassRate:成功率:$STEP_ROBOT_PASSRATE:Default Report:http://testing.rdc.aliyun.com/assets/$STEP_REPORT_URL
+redline Passed:成功:$STEP_ROBOT_PASS:Success Failed:失败:$STEP_ROBOT_FAILED:Error PassRate:成功率:$STEP_ROBOT_PASSRATE:Default Report:$STEP_REPORT_URL
